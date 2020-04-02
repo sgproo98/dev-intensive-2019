0059596e45ca95c4aae9b071a993c5311692dfa5
sgproo98 <mrgeneralgeneral@gmail.com>
2020-04-02 16:27:14 +0300

    add Build pattern

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -52,5 +52,27 @@ data class User(
             return User(id = "$lastId", firstName = firstName, lastName = lastName)
         }
     }
+
+    class Builder{
+        var id: String = ""
+        var firstName: String? = null
+        var lastName: String? = null
+        var avatar: String? = null
+        var rating: Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = null
+        var isOnline: Boolean = false
+
+        fun id(s: String) = apply { this.id = s }
+        fun firstName(s: String) = apply { this.firstName = s }
+        fun lastName(s: String) = apply { this.lastName = s }
+        fun avatar(s: String) = apply { this.avatar = s }
+        fun rating(n: Int) = apply { this.rating = n }
+        fun respect(n: Int) = apply { this.respect = n }
+        fun lastVisit(d: Date) = apply { this.lastVisit = d }
+        fun isOnline(b: Boolean) = apply { this.isOnline = b }
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
+
+    }
 }
 
